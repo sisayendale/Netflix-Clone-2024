5523d9fb39973ceaec16a016758579692cbd901b
sisay endale <sisayendale2023@gmail.com>
2024-04-29 17:58:58 -0600

    banner

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -1,7 +1,52 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import replace from "@rollup/plugin-replace";
+import envcomatible from "vite-plugin-env-compatible";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  envPrefix: "REACT_APP_", // Keep for clarity
+  plugins: [
+    react(),
+    envcomatible(),
+    replace({
+      "process.env": JSON.stringify(process.env),
+      "process.env.REACT_APP_API_KEY": JSON.stringify(
+        process.env.REACT_APP_API_KEY
+      ),
+      preventAssignment: true,
+    }),
+  ],
+});
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import envcomatible from "vite-plugin-env-compatible";
+// import replace from "@rollup/plugin-replace";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   envPrefix: "REACT_APP_",
+//   plugins: [
+//      react(),
+//   envcomatible(),
+// replace({
+//   "process.env.REACT_APP_API_KEY": JSON.stringify(
+//     process.env.REACT_APP_API_KEY
+//   ),
+// }),
+// replace({
+//   "process.env": JSON.stringify(process.env),
+//   preventAssignment: true, // Explicitly set preventAssignment to true
+// }),
+//   ],
+// });
+
+// import replace from '@rollup/plugin-replace';
+
+// export default {
+//   plugins: [
+//     replace({
+//       'process.env.REACT_APP_API_KEY': JSON.stringify(process.env.REACT_APP_API_KEY)
+//     })
+//   ]
+// };
