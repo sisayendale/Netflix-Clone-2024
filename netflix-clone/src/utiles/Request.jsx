24b1a831a3b3e20f58a1b2c3e32c1cdf6566733f
sisayendale <156632212+sisayendale@users.noreply.github.com>
2024-04-29 13:41:20 -0600

    Update Request.jsx

--- netflix-clone/src/utiles/Request.jsx
+++ netflix-clone/src/utiles/Request.jsx
@@ -1,4 +1,4 @@
-//const API_KEY = "7c3d8f8d7d8f8d7d8f8d7d8f";
+
 const API_KEY = process.env.REACT_APP_API_KEY;
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
