5df91d5cfbeea8ce0ee084d4d84f3cb0249a6342
sisay endale <sisayendale2023@gmail.com>
2024-05-02 06:09:36 -0600

    update deploye

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -2,18 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import replace from "@rollup/plugin-replace";
 import envcomatible from "vite-plugin-env-compatible";
-import { VitePWA } from "vite-plugin-pwa";
-import { VitePluginGhPages } from "vite-plugin-gh-pages";
 
 export default defineConfig({
   envPrefix: "REACT_APP_", // Keep for clarity
+  base: "/netflix-clone/",
   plugins: [
     react(),
-    VitePWA(),
-    VitePluginGhPages({
-      branch: "gh-pages",
-      dir: "dist",
-    }),
     envcomatible(),
     replace({
       "process.env.REACT_APP_API_KEY": JSON.stringify(
