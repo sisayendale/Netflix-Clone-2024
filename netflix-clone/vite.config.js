5fc93b2aaca33a2a514ee0e7ce311d1ef53da8e9
sisay endale <sisayendale2023@gmail.com>
2024-05-06 09:57:37 -0600

    deploye_update

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -1,18 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import replace from "@rollup/plugin-replace";
-import envcomatible from "vite-plugin-env-compatible";
 
 export default defineConfig({
-  envPrefix: "REACT_APP_", // Keep for clarity
+  // envPrefix: "REACT_APP_", // Keep for clarity
   base: "/netflix-clone/",
   plugins: [
     react(),
-    envcomatible(),
-    replace({
-      "process.env.REACT_APP_API_KEY": JSON.stringify(
-        process.env.REACT_APP_API_KEY
-      ),
-    }),
   ],
 });
