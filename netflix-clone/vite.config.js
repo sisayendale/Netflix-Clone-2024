5a96cee06c43b1f2c8071a7fd8294c74c532e0c0
sisay endale <sisayendale2023@gmail.com>
2024-05-13 07:55:10 -0600

    deplyingProject

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   // envPrefix: "REACT_APP_", // Keep for clarity
-  base: "/netflix-clone/",
+  // base: "/netflix-clone/",
   plugins: [
     react(),
   ],
